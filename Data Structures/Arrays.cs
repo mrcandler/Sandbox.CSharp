0ed426a454a556f949babf2f3d09e97918b8c38a
Donta Candler <mrcandler@live.com>
2026-03-01 18:49:21 -0800

    Add Arrays class with examples of array declaration and initialization; create Namespace file

--- Data Structures/Arrays.cs
+++ Data Structures/Arrays.cs
@@ -1,3 +1,19 @@
-public class Arrays {
+public class Arrays 
+{
+    public static void Main()
+    {
+        // How to declare an array without initializing it?
+        int[] uninitializedArray;
 
-}
+        // How to declare an array with a specific size only?
+        int[] numbersWithSize = new int[5];
+
+        // How to declare an empty array?
+        int[] emptyArray = new int[0];
+
+        // How to declare and initialize an array with values?
+        int[] intxv = { 1, 2, 3, 4, 5 };
+
+    }   
+}   
+
